292132a0590ea8a100936a73afa902656179ab99
Tallia-Sousa <talliasousa.com@gmail.com>
2024-04-06 14:32:36 -0300

    Ultimo commit do roteiro seguido

--- Money.java
+++ Money.java
@@ -1,6 +1,6 @@
-abstract class Money {
+class Money {
 
-        protected int amount;
+    protected int amount;
     protected String currency;
 
 
@@ -10,17 +10,17 @@ abstract class Money {
     }
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
 
 
     static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount,"CHF");
+        return new Money(amount, "CHF");
     }
 
 
@@ -28,7 +28,8 @@ abstract class Money {
         return currency;
     }
 
-    abstract Money times(int multiplier);
-
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
 }
