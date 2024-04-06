9dec2cdf365fe0d13bcb2825d84fda8ec317e25b
Tallia-Sousa <talliasousa.com@gmail.com>
2024-04-06 14:23:18 -0300

    Roteiro  Commit 4

--- Money.java
+++ Money.java
@@ -1,7 +1,13 @@
 abstract class Money {
 
         protected int amount;
+    protected String currency;
 
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
@@ -10,12 +16,19 @@ abstract class Money {
 
 
     static Money dollar(int amount)  {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount,"CHF");
+    }
+
+
+    String currency() {
+        return currency;
     }
 
     abstract Money times(int multiplier);
+
+
 }
--- TestMoney.java
+++ TestMoney.java
@@ -30,5 +30,11 @@ public void testMultiplication() {
         assertEquals(Money.franc(15), five.times(3));
     }
 
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
 
 }
