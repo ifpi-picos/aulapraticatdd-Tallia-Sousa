cfe7966c6e21102e0654d26f87e51d5cf0e0ce73
Tallia-Sousa <talliasousa.com@gmail.com>
2024-04-06 11:38:30 -0300

    Roteiro Passo 2 E Commit 2

--- Dollar.java
+++ Dollar.java
@@ -1,6 +1,12 @@
-class Dollar {
+class Dollar extends Money {
 
-    int amount = 10;
-    Dollar(int amount) {}
-    void times(int multiplier) {}
-}
+    Dollar(int amount) {
+        this.amount= amount;
+    }
+
+        Dollar times(int multiplier) {
+            return new Dollar(amount * multiplier);
+        }
+
+
+    }
